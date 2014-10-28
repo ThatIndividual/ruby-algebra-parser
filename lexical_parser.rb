58e9214b7b0f40bdf12ddab989177a436cc91568
koalemos <gabriel.nv.ionescu@gmail.com>
2014-10-28 22:24:51 +0000

    Can now tokenize basic math operators: +,-,*,/ and parens; The lexer does some basic grammar checks, like checking for equal number of parens; Subunitary numbers as "0.123" may be written as ".123"

--- lexical_parser.rb
+++ lexical_parser.rb
@@ -9,48 +9,77 @@ class Token
   end
 end
 
-def lexical_parse(input_string)
-  input = input_string
+def lexical_parse(parser_input)
   parser_state = []
   parser_output = []
   current_token = ""
 
-  while input != ""
-    if input.match('^[0-9]')
-      parser_state.push(:integer)
-      while input.match('^[0-9.]')
-        if input.match('^[0-9]')
-          current_token += input[0]
-          input[0] = ''
-        elsif input.match('^\.')
-          case parser_state.last
-          when :integer
-            parser_state.pop
-            parser_state.push(:float)
-            current_token += "."
-            input[0] = ''
-          when :float
-            print "=== Parser Error ===\n" + 
-              "A float may contain only one decimal point\n"
+  while parser_input != ""
+    case parser_input
+    when /^[0-9.]/
+      while(parser_input.match(/^[0-9.]/))
+        if(parser_input.match(/^[0-9]/))
+          if(parser_state.last != :integer && parser_state.last != :float)
+            parser_state.push(:integer)
+          end
+          current_token += parser_input[0]
+          parser_input[0] = ''
+        elsif(parser_input.match(/^\./))
+          if(parser_state.last == :integer)
+            current_token += parser_input[0]
+            parser_state[-1] = :float
+            parser_input[0] = ''
+          elsif(parser_state.last == :float)
+            print "=== Parser Error ===\n"
+            print "A float may contain only one decimal point\n"
             exit
           else
-            print "=== Parser Error ===\n" +
-              "Expected :integer or :float when reading decimal place\n" +
-              "Got #{parser_state.last}\n"
+            parser_state.push(:float)
+            current_token += "0."
+            parser_input[0] = ''
           end
         end
       end
       parser_output.push(Token.new(current_token, parser_state.pop))
-      current_token = ''
+      current_token = ""
+    when /^[+*\/]/
+      parser_output.push(Token.new(parser_input[0], :operator))
+      parser_input[0] = ''
+    when /^[-]/
+      if(parser_output.last.type==:operator && parser_input.match(/^-[0-9.]/))
+        current_token = "-"
+      else
+        parser_output.push(Token.new("-", :operator))
+      end
+      parser_input[0] = ''
+    when /^[(]/
+      parser_state.push(:parens_open)
+      parser_output.push(Token.new("(", :parens_open))
+      parser_input[0] = ''
+    when /^[)]/
+      if(parser_state.last == :parens_open)
+        parser_state.pop
+        parser_output.push(Token.new(")", :parens_close))
+        parser_input[0] = ''
+      else
+        print "=== Parser error ===\n"
+        print "The expression has too many closing parens\n"
+        exit
+      end
     else
-      input[0] = ''
+      parser_input[0] = ''
     end
   end
 
+  if(parser_state.last == :parens_open)
+    print "=== Parser error ===\n"
+    print "The expression has too many opening parens\n"
+    exit
+  end
   return parser_output
 end
 
-tokenized = lexical_parse("12*(3.5+4.5)-12/6")
+tokenized = lexical_parse("12*-1*(3.5+4.5)-12/6")
 tokenized.each do |x|
   p x
 end
